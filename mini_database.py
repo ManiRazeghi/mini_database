68d2e41c2d33fef06f9df012c8e09ab7814a4604
mani <manirazeghi049@gmail.com>
2025-01-01 08:42:15 -0800

    delete method

--- mini_database.py
+++ mini_database.py
@@ -16,7 +16,7 @@ class Database:
     def __init__(self, table_name: str) -> None:
         self.table_name = table_name
         self.address = f'{os.getcwd()}/database/{self.table_name}.csv'
-    
+
     
     def add_column(self, columns_data: list[str]) -> None:
         
@@ -26,6 +26,7 @@ class Database:
         with open(self.address, 'w', newline= '') as table_csv:
             columns_data.insert(0, 'id')
             csv.writer(table_csv).writerow(columns_data)
+        
 
 
     def add_one_row(self, row_data: list[str]) -> None:
@@ -37,9 +38,30 @@ class Database:
 
             row_data.insert(0, num_id)
             csv.writer(table_csv).writerows([row_data])
-            table_csv.seek(len(row_data))
 
     
+    def delete(self, column_data: dict) -> None:
+        collected = []
+
+        with open(self.address, 'r') as table_csv:
+
+            for item in csv.DictReader(table_csv):
+                mark = 0
+
+                for value in column_data.values():
+                    if value in item.values():
+                        mark += 1
+                
+                if mark != len(column_data):
+                    collected.append(list(item.values())[1:])
+            
+            columns = list(item.keys())[1:]
+        os.remove(self.address)
+
+        self.add_column(columns)
+        for row in collected:
+            self.add_one_row(row)
+
 
 
 
