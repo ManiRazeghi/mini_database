e452ebb31379904794ebfc285f87532901ba7cdd
mani <manirazeghi049@gmail.com>
2025-01-01 07:19:00 -0800

    add_one_row and update add_column

--- mini_database.py
+++ mini_database.py
@@ -15,15 +15,33 @@ class Database:
 
     def __init__(self, table_name: str) -> None:
         self.table_name = table_name
+        self.address = f'{os.getcwd()}/database/{self.table_name}.csv'
     
     
     def add_column(self, columns_data: list[str]) -> None:
         
-        if os.path.exists(address := f'{os.getcwd()}/database/{self.table_name}.csv'):
-            raise FileExistsError(f'{address} is already exists.')
+        if os.path.exists(self.address):
+            raise FileExistsError(f'{self.address} is already exists.')
 
-        with open(address, 'w', newline= '') as table_csv:
+        with open(self.address, 'w', newline= '') as table_csv:
+            columns_data.insert(0, 'id')
             csv.writer(table_csv).writerow(columns_data)
+
+
+    def add_one_row(self, row_data: list[str]) -> None:
+
+        num_id = 0
+        with open(self.address, 'r+', newline= '') as table_csv:
+            for _ in csv.reader(table_csv):
+                num_id += 1
+
+            row_data.insert(0, num_id)
+            csv.writer(table_csv).writerows([row_data])
+            table_csv.seek(len(row_data))
+
+    
+
+
 
 
 
