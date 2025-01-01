c2355d19189bd59b635d37036c13b136cee70843
mani <manirazeghi049@gmail.com>
2025-01-01 06:23:41 -0800

    update gitignore and module

---
+++ mini_database.py
@@ -0,0 +1,18 @@
+'''This module has features that you can make simple database for your programms.'''
+
+# import requirements
+import csv
+import os
+
+
+# check that database folder is exists.
+if not os.path.exists(f'{os.getcwd()}/database'):
+    os.mkdir(f'{os.getcwd()}/database')
+
+
+
+class Database:
+
+    pass
+
+
